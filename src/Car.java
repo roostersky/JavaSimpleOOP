eb8f9173b5f2eb07bfa1818596b102911b102452
lodomur <gmstudio1916@gmail.com>
2016-02-15 12:09:43 +0100

    Add new elements to program

--- src/Car.java
+++ src/Car.java
@@ -3,16 +3,14 @@ public class Car {
 
 	// Variables
 	int price;
-	int number;
+	int carNumber;
 	int counter;
+	static int count;
+	char valute;
 	String model;
 	String company_creator;
 	String country_creator;
 
-	public static void main(String[] args) {
-
-	}
-	
 	// getPrice
 	public int getPrice() {
 		return price;
@@ -20,12 +18,17 @@ public class Car {
 
 	// getNumber
 	public int getNumber() {
-		return number;
+		return carNumber;
 	}
 
 	// getCounter
 	public int getCounter() {
 		return counter;
+	}
+
+	// getValute
+	public char getValute() {
+		return valute;
 	}
 
 	// getModel
@@ -44,16 +47,22 @@ public class Car {
 	}
 
 	// Constructor
-	public Car(int price, int number, int counter, String model, String company_creator, String country_creator) {
+	public Car(int price, char valute, int carNumber, String model, String company_creator, String country_creator) {
 		this.price = price;
-		this.number = number;
+		this.carNumber = carNumber;
 		this.counter = counter;
 		this.model = model;
+		this.valute = valute;
 		this.company_creator = company_creator;
 		this.country_creator = country_creator;
+		count++;
+		counter = count;
 
 	}
-	
-	
+
+	public void showInfoAboutCar() {
+		System.out.println(counter + ") CREATOR " + company_creator + " MODEL " + model + " COUNTRY " + country_creator
+				+ " CAR NUMBER " + carNumber + " PRICE " + price + " " + valute);
+	}
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,41 @@
+import java.util.ArrayList;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Car car = new Car();
+		
+		// Simple text 
+		System.out.println("CARS TABLE LIST ");
+		System.out.println(" ");
+
+		// Creat car objects
+		Car car1 = new Car(475490, '£', 564345, "Sence", "Fiat", "French");
+		Car car2 = new Car(764435, '$', 566545, "Reno", "Duster", "French");
+		Car car3 = new Car(764435, '₴', 566545, "Reno", "Duster", "French");
+		Car car4 = new Car(764435, '£', 566545, "Reno", "Duster", "French");
+		Car car5 = new Car(764435, '£', 566545, "Reno", "Duster", "French");
+		Car car6 = new Car(764435, '₴', 566545, "Reno", "Duster", "French");
+		Car car7 = new Car(764435, '$', 566545, "Reno", "Duster", "French");
+		
+		// Creat carList
+		ArrayList<Car> carList = new ArrayList<>();
+		
+		// Add car object to carList
+		carList.add(car1);
+		carList.add(car1);
+		carList.add(car1);
+		carList.add(car1);
+		carList.add(car1);
+		carList.add(car1);
+		carList.add(car1);
+		
+		
+		// Show info about cars from carList
+		for (int i = 0; i < carList.size(); i++) {
+			carList.get(i).showInfoAboutCar();
+		}
+
+		
 	}
 
 }
